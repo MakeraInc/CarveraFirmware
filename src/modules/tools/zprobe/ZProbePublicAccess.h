ff8b68a71ad2082fd2c6fd98b8914b88b2b4244e
redwolf010 <56901084@qq.com>
2024-09-09 19:06:23 +0800

    Create ZProbePublicAccess.h

---
+++ src/modules/tools/zprobe/ZProbePublicAccess.h
@@ -0,0 +1,9 @@
+#ifndef __ZPROBEPUBLICACCESS_H_
+#define __ZPROBEPUBLICACCESS_H_
+
+// addresses used for public data access
+#define zprobe_checksum    CHECKSUM("zprobe")
+#define get_zprobe_pin_states_checksum CHECKSUM("zprobe_pin_states")
+#define get_zprobe_time_checksum CHECKSUM("zprobe_time")
+
+#endif
