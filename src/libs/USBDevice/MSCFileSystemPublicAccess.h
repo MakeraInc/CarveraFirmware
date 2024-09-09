9f68c2ce1f068515b4e3d1e077bd4822340d0c51
redwolf010 <56901084@qq.com>
2024-09-09 19:11:24 +0800

    Create MSCFileSystemPublicAccess.h

---
+++ src/libs/USBDevice/MSCFileSystemPublicAccess.h
@@ -0,0 +1,11 @@
+#ifndef MSCFILESYSTEMPUBLICACCESS_H
+#define MSCFILESYSTEMPUBLICACCESS_H
+
+#include "checksumm.h"
+#include <string>
+
+#define msc_file_system_checksum   	CHECKSUM("msc_file_system")
+
+#define check_usb_host_checksum		CHECKSUM("check_usb_host")
+
+#endif
