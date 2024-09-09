283e27f86f6ea616f5562785a2851f4d41e3b5cd
redwolf010 <56901084@qq.com>
2024-09-09 19:06:12 +0800

    Create MainButtonPublicAccess.h

---
+++ src/modules/utils/mainbutton/MainButtonPublicAccess.h
@@ -0,0 +1,13 @@
+#ifndef MAINBUTTONPUBLICACCESS_H
+#define MAINBUTTONPUBLICACCESS_H
+
+#include "checksumm.h"
+#include <string>
+
+#define main_button_checksum   		CHECKSUM("main_button")
+
+#define switch_power_12_checksum	CHECKSUM("switch_power_12")
+#define switch_power_24_checksum	CHECKSUM("switch_power_24")
+#define get_e_stop_state_checksum	CHECKSUM("get_e_stop_state")
+
+#endif
