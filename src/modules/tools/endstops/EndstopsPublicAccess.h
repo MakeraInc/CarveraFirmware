7c3f4b283b8c327224e0e244442d9e2968736caa
redwolf010 <56901084@qq.com>
2024-11-18 10:48:56 +0800

    Update EndstopsPublicAccess.h

--- src/modules/tools/endstops/EndstopsPublicAccess.h
+++ src/modules/tools/endstops/EndstopsPublicAccess.h
@@ -9,6 +9,7 @@
 #define get_homing_status_checksum CHECKSUM("homing_status")
 #define get_homed_status_checksum CHECKSUM("homed_status")
 #define get_endstop_states_checksum CHECKSUM("endstop_states")
+#define get_endstopAB_states_checksum CHECKSUM("endstopAB_states")
 #define get_cover_endstop_state_checksum CHECKSUM("cover_endstop_state")
 
 #endif
