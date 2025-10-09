23e19722e397223a9c4bd42e1964f118922f0c5e
Adam Jensen <adam@acj.sh>
2025-10-08 21:32:43 -0400

    Use a default version string when a git branch isn't checked out

--- src/generate-version.sh
+++ src/generate-version.sh
@@ -1,2 +1,7 @@
-echo `git symbolic-ref HEAD 2> /dev/null | cut -b 12-`-`git rev-parse --short=7 HEAD`
+current_branch=`git symbolic-ref HEAD 2> /dev/null | cut -b 12-`
+current_commit=`git rev-parse --short=7 HEAD`
+if [ -z "$current_branch" ]; then
+  current_branch="2.0.0c"
+fi
+echo "$current_branch-$current_commit"
 touch version.cpp
