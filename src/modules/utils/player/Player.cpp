02ab4ff86bac3242be68b5e8d087c0e60aa0ab84
lishufeng <lisf@makera.com>
2026-05-25 17:12:22 +0800

    Update Player.cpp
    When the zeroing operation is not completed, playing is prohibited.

--- src/modules/utils/player/Player.cpp
+++ src/modules/utils/player/Player.cpp
@@ -340,6 +340,10 @@ void Player::play_command( string parameters, StreamOutput *stream )
 //		return;
 //	}
 
+	if (!THEROBOT->is_homed_all_axes()) {
+		return;
+	}
+
     // extract any options from the line and terminate the line there
     string options= extract_options(parameters);
     // Get filename which is the entire parameter line upto any options found or entire line
