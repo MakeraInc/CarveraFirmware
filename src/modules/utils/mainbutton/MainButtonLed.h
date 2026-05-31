782d5cf78078c4b21abe80557bb76b194977a8fc
Konstantin Tcepliaev <f355@f355.org>
2026-05-31 19:27:14 +0200

    Extract main button LED writer

---
+++ src/modules/utils/mainbutton/MainButtonLed.h
@@ -0,0 +1,9 @@
+#ifndef MAINBUTTON_LED_H
+#define MAINBUTTON_LED_H
+
+void mainbutton_led_write_strip(unsigned char R1, unsigned char G1, unsigned char B1, unsigned char R2,
+                                unsigned char G2, unsigned char B2, unsigned char R3, unsigned char G3,
+                                unsigned char B3, unsigned char R4, unsigned char G4, unsigned char B4,
+                                unsigned char R5, unsigned char G5, unsigned char B5);
+
+#endif
